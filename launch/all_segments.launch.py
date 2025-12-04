30bbc8cf1f275f077872d7414186318d8bed265d
Rowan Border <rowanjborder@gmail.com>
2025-12-04 19:13:13 +0200

    update vicon launch

--- launch/all_segments.launch.py
+++ launch/all_segments.launch.py
@@ -1,17 +1,19 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
 
 def generate_launch_description():
-
     vicon_computer_ip = "192.168.0.101" # ip address of vicon computer
     port_number = "801" # default port number
     host_name = f"{vicon_computer_ip}:{port_number}"
+    uav_namespace = os.environ.get("UAV_NAME", "")
 
     return LaunchDescription([
         Node(
             package='vicon_bridge',
             executable='vicon_bridge',
             name='vicon_bridge',
+            namespace=uav_namespace,
             parameters = [
                 {"host_name": host_name},
             #    {"stream_mode": "ServerPush"}, # or "ClientPull"
